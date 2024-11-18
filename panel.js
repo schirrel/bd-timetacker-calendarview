d46c433f78d0ab5b1c0ce3d924ba639310a475ee
Alan Schio <6757777+schirrel@users.noreply.github.com>
2024-11-18 11:41:13 -0400

    feat(panel): remove console and comment

--- panel.js
+++ panel.js
@@ -2,9 +2,7 @@ chrome.devtools.network.onRequestFinished.addListener((request) => {
   request.getContent((body) => {
     if (request.request && request.request.url) {
       if (request.request.url.includes("api/v1/employees/records")) {
-        //continue with custom code
         var bodyObj = JSON.parse(body); //etc.
-        console.log(bodyObj);
         chrome.runtime.sendMessage({
           type: "bdExtCalendarData",
           response: bodyObj,
